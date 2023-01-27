1fdc63d4a36fab194acc3af834925acf0a15187d
Sadhana Ramani <sdramani@usc.edu>
2023-01-27 06:50:33 +0000

    finished part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,29 @@
 #include <sstream>
 
 #include "ulliststr.h"
+using namespace std;
 
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+ULListStr dat;
+for (int i = 0; i < 10; i++){
+	dat.push_back(to_string(i));
+	//cout << "get " << i << ": " << dat.get(i) << endl;
+}
+cout << dat.get(8) << endl;
+dat.pop_back();
+cout << dat.get(8) << endl;
+
+/**cout << "hi";
+dat.push_front("0");
+dat.push_front("1");
+dat.push_front("2");
+cout  << dat.get(0) << endl; 
+cout << dat.get(1) << endl;
+cout << dat.get(2) << endl;
+**/ //prints: 8 7 9
+cout << dat.size() << endl;  // prints 3 since there are 3 strings stored
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,7 +1,10 @@
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
+#include <sstream>
 
+using namespace std;
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -33,9 +36,9 @@ void ULListStr::push_back(const std::string& val)
     head_ = new Item();
     tail_ = head_;
 
-    (head_->val)[0] == val;
+    (head_->val)[0] = val;
     head_->last = 1;
-
+    //cout << "is it working?:" <<(head_->val)[0]<< endl;
     size_++;
     return;
   }
@@ -48,11 +51,15 @@ void ULListStr::push_back(const std::string& val)
     tail_->prev = currItem;
     currItem->next = tail_;
     tail_->val[0] = val;
+    tail_->first = 0;
+    tail_->last = 1;
+    size_++;
 
   }
 
   else {
     tail_->val[(tail_->last)] = val;
+    //cout << "is it working?:" << tail_->val[(tail_->last)] << endl;
     tail_->last++;
     size_++;
   }
@@ -65,9 +72,11 @@ void ULListStr::push_front(const std::string& val)
     head_ = new Item();
     tail_ = head_;
 
-    (head_->val)[ARRSIZE-1] == val;
+    head_->val[ARRSIZE-1] = val;
+    //cout << head_->val[ARRSIZE-1] << endl;
     head_->last = ARRSIZE;
     head_->first = ARRSIZE-1;
+    
 
     size_++;
     return;
@@ -81,12 +90,19 @@ void ULListStr::push_front(const std::string& val)
     head_->next = currItem;
     currItem->prev = head_;
     head_->val[ARRSIZE-1] = val;
+    head_->first = 9;
+    head_->last = ARRSIZE;
+    //cout << "push_front:" << head_val[head_->first] <<endl;
+
+    size_++;
 
   }
 
   else {
-    head_->val[(head_->first - 1)] = val;
+    //cout << "default" << endl;
     head_->first--;
+    head_->val[(head_->first)] = val;
+    cout << "val: " << head_->val[(head_->first)] << endl;
     size_++;
   }
 }
@@ -116,24 +132,41 @@ void ULListStr::pop_front()
 
 void ULListStr::pop_back()
 {
+  cout << "tail_last:" <<tail_->last<<endl;
   if (tail_->last == 1){
     Item* temp_ = tail_;
     tail_ = tail_->prev;
     delete temp_;
+    
+    cout <<"c" <<size_;
     size_--;
+    cout << "c"<<size_;
+    
     if (tail_ == nullptr){
       head_ = nullptr;
     }
+    
     else{
       tail_->next = nullptr;
     }
   }
 
   else{
-    tail_->val[tail_->last] = "";
+    cout << "hi";
+    tail_->val[tail_->last - 1] = "";
     tail_->last--;
     size_ -= 1;
   }
+}
+
+std::string const & ULListStr::front() const
+{
+  return head_->val[head_->first];
+}
+
+std::string const & ULListStr::back() const
+{
+  return tail_->val[(tail_->last)-1];
 }
 
 void ULListStr::set(size_t loc, const std::string& val)
@@ -176,23 +209,28 @@ void ULListStr::clear()
 
 std::string* ULListStr::getValAtLoc(size_t loc) const
 {
-  if (loc >= size_){
+  if (loc > size_){
     return NULL;
   }
 
   else{
     size_t counter = 0;
     Item* currItem = head_;
+    size_t place = currItem->first;
+    //cout << "place:" << place << endl;
     while (counter < loc){
-      if ((currItem->last - currItem->first) + counter >= loc){
-        counter += (currItem->last - currItem->first);
+      if (place == ((currItem->last) - 1)){
+        //cout << "place1:" << place << " " << endl;
         currItem = currItem->next;
-        continue;
+        place = currItem->first;
+        counter++;
+        //cout << "place2:" << place << " " << endl;
       }
-
       else{
-        return &(currItem->val[loc - counter]);
+        counter++;
+        place++;
       }
     }
+    return &(currItem->val[place]);
   }
 }
